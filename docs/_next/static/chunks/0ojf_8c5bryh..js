3d968e1ffb8514c62a94fc2ba3687e3bd0d81a2e
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-04-04 12:03:57 +0700

    add prisma

---
+++ docs/_next/static/chunks/0ojf_8c5bryh..js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/0-rstrm50bfe2.js",
+  "static/chunks/0y~znvr8j7ty_.js",
+  "static/chunks/08p6yi7j43jrx.js",
+  "static/chunks/05v3nyetqcz8e.css",
+  "static/chunks/0qg3o3ls85~o7.css",
+  "static/chunks/turbopack-0ylyh.6uj4yyr.js"
+])
